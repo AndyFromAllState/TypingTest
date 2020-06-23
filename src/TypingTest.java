d48c5f16bdcf78708ffd26c8fddd7ff6505b6ea1
andyl <andyl@192.168.10.70>
2020-06-23 14:09:47 -0700

    Added KeyListner

--- src/TypingTest.java
+++ src/TypingTest.java
@@ -11,8 +11,11 @@ import javax.swing.text.StyledDocument;
 public class TypingTest extends JFrame {
 	private final int FRAME_HEIGHT = 800, FRAME_WIDTH = 1350;
 	private JTextPane testPanel = new JTextPane();
+	private JTextPane textInputPanel = new JTextPane();
 	
 	public TypingTest() {
+		KeyboardListener KL = new KeyboardListener();
+		
 		JPanel panel = new JPanel();
 		this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,19 +32,55 @@ public class TypingTest extends JFrame {
 		StyledDocument doc = testPanel.getStyledDocument();
 		SimpleAttributeSet center = new SimpleAttributeSet();
 		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-		doc.setParagraphAttributes(0, doc.getLength(), center, false);  
-		
+		doc.setParagraphAttributes(0, doc.getLength(), center, false);
+		 
 		//puts the testPanel into a scrollable pane
 		JScrollPane scrollable = new JScrollPane (testPanel);
 		scrollable.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollable.setPreferredSize(new Dimension(FRAME_WIDTH - 20, FRAME_HEIGHT/2));
 		
-		panel.add(scrollable);
+		testPanel.setPreferredSize(new Dimension(FRAME_WIDTH - 20, FRAME_HEIGHT/2));
 		
+		textInputPanel.setFont(font);
+		textInputPanel.setPreferredSize(new Dimension(FRAME_WIDTH - 20, FRAME_HEIGHT/2));
+		textInputPanel.addKeyListener(KL);
+		
+		panel.add(testPanel);
+		panel.add(textInputPanel);
 		this.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
 		new TypingTest();
+	}
+	
+	private class KeyboardListener implements KeyListener {
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			if (e.getKeyChar() == testPanel.getText().charAt(0)) {
+				textInputPanel.setEditable(true);
+				testPanel.setText(testPanel.getText().substring(1));
+				System.out.println("hi");
+			}
+			else if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE)
+				textInputPanel.setEditable(false);
+			else
+				textInputPanel.setEditable(false);
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 }
